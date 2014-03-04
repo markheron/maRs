0245912031a1ba1b2b84c86e52754ce6784c0c50
Mark Heron <heron@genzentrum.lmu.de>
2014-03-04 11:11:37 +0100

    - refactored smear to remove negation of from/to  - renamed smear to smear_simple  - implemented smear that recursivly uses iterative smoothing to speed up the process

--- R/smear.R
+++ R/smear.R
@@ -7,6 +7,31 @@
 NULL
 
 
+##' smear_simple
+##'
+##' Computes a smeared vector, basically a running_sum.
+##' 
+##' add's the vector on its self several times, so good for short smears of long vectors
+##'
+##' @param x vector to smear
+##' @param from where to start
+##' @param to where to go
+##' @return smeared ff vector
+smear_simple <- function(x, from=0, to=1) {
+  
+  if(from > to) {
+    return(smear_simple(x,to,from))
+  }
+  
+  len <- length(x)
+  smeared <- rep(0,len)
+  for(i in from:to) {
+    smeared <- smeared + c(rep(0,max(i,0)),x[max(1-i,1):min(len-i,len)],rep(0,-min(i,0)))
+  }
+  return(smeared)
+}
+
+
 ##' smear
 ##'
 ##' Computes a smeared vector, basically a running_sum.
@@ -20,13 +45,32 @@ NULL
 ##' @return smeared ff vector
 smear <- function(x, from=0, to=1) {
   
-  from <- -from
-  to <- -to
+  if(from > to) {
+    return(smear(x,to,from))
+  }
   
-  len <- length(x)
-  smeared <- rep(0,len)
-  for(i in from:to) {
-    smeared <- smeared + c(rep(0,-min(i,0)),x[max(i+1,1):min(len+i,len)],rep(0,max(i,0)))
+  smooth_len <- to-from+1
+  times <- floor(log2(smooth_len))-1
+  
+  smeared <- c(rep(0,max(0,from)), x, rep(0,max(0,-from)))
+  len_smear <- length(smeared)
+  if(times >= 0) {
+    for(iter in 0:times) {
+      i <- 2^iter
+      smeared <- smeared + c(rep(0,i),smeared[1:(len_smear-i)])
+    }
+  }
+  smeared <- smeared[(max(1,1-from)):min(len_smear, len_smear-from)]
+  
+  new_from <- (from+2^floor(log2(smooth_len)))
+  if(new_from <= to) {
+    
+    smeared <- smeared + smear(x, new_from, to)
+    
+#     len_x <- length(x)
+#     for(i in ((from+2^floor(log2(smooth_len))):to)) {    #from:to) {
+#       smeared <- smeared + c(rep(0,max(i,0)),x[max(1-i,1):min(len_x-i,len_x)],rep(0,-min(i,0)))
+#     }
   }
   return(smeared)
 }
